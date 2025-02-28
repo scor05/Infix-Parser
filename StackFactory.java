48ef7b97b8874a89f590b5c38e3d8979f7962f2e
Samuuu132 <gud24451@uvg.edu.gt>
2025-02-27 20:28:25 -0600

    Se crea la clase stackfactory

---
+++ StackFactory.java
@@ -0,0 +1,26 @@
+public class StackFactory {
+    /**
+     * 
+     * @param type
+     * @return
+     */
+    public static IStack<Integer> getStack(String type) {
+        switch (type.toLowerCase()) {
+            case "arraylist": return new ArrayListStack<>();
+            case "vector": return new VectorStack<>();
+            default: throw new IllegalArgumentException("Tipo de Stack no válido: " + type);
+        }
+    }
+/**
+ * 
+ * @param type
+ * @return
+ */
+    public static IStack<Integer> getListStack(String type) {
+        switch (type.toLowerCase()) {
+            case "single": return new ListStack<>(new SingleLinkedList<>());
+            case "double": return new ListStack<>(new DoubleLinkedList<>());
+            default: throw new IllegalArgumentException("Tipo de Lista no válido: " + type);
+        }
+    }
+}
