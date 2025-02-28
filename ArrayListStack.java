72af3a49fe052c625d0f208bb613808386c5f9cf
Juan Salguero <sal24374@uvg.edu.gt>
2025-02-27 20:10:36 -0600

    Creacion de clase ArrayListStack

---
+++ ArrayListStack.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class ArrayListStack<T> implements IStack<T> {
+    private final ArrayList<T> stack = new ArrayList<>();
+    
+    @Override
+    public void push(T item) { stack.add(item); }
+    @Override
+    public T pop() { return stack.isEmpty() ? null : stack.remove(stack.size() - 1); }
+    @Override
+    public T peek() { return stack.isEmpty() ? null : stack.get(stack.size() - 1); }
+}
