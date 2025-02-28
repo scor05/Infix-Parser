af0f4c67854960b2bce3737c539311c140e0c726
Juan Salguero <sal24374@uvg.edu.gt>
2025-02-27 20:14:57 -0600

    Creacion de clase con el patron Factory

---
+++ ListFactory.java
@@ -0,0 +1,14 @@
+public class ListFactory {
+    /**
+     *  
+     * @param type
+     * @return
+     */
+    public static IList<Integer> getList(String type) {
+        return switch (type.toLowerCase()) {
+            case "single" -> new SingleLinkedList<>();
+            case "double" -> new DoubleLinkedList<>();
+            default -> new SingleLinkedList<>();
+        };
+    }
+}
