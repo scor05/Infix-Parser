3bda8f66849de160ca8bc27cc8f5f98981ae87b5
Samuuu132 <gud24451@uvg.edu.gt>
2025-02-27 20:08:10 -0600

    Creacion de la clase de liststack

---
+++ ListStack.java
@@ -0,0 +1,14 @@
+public class ListStack<T> implements IStack<T> {
+    private final IList<T> list;
+    
+    public ListStack(IList<T> list) {
+        this.list = list;
+    }
+
+    @Override
+    public void push(T item) { list.add(item); }
+    @Override
+    public T pop() { return list.remove(); }
+    @Override
+    public T peek() { return null; } 
+}
