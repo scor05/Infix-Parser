2e7884f80cf99812ec42b4ba7955278b8d825887
Santiago Cordero <santicordero28@gmail.com>
2025-02-27 21:11:05 -0600

    Tests de las implementaciones de los stacks creados.

--- infixparser/src/test/java/infixparser/StackImplementationTests.java
+++ infixparser/src/test/java/infixparser/StackImplementationTests.java
@@ -1,11 +1,31 @@
 package infixparser;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 public class StackImplementationTests {
     @Test
     public void shouldReturnArrayListStackResult() {
-        IStack<Integer> stack = new ArrayListStack<>();
-        Calculadora.getInstance().evaluate("1 2 + 3 4 * +", stack);
+        int resultado = Calculadora.getInstance().evaluate("1 2 +", new ArrayListStack<Integer>());
+        assertEquals(resultado, 3);
+    }
+
+    @Test
+    public void shouldReturnVectorStackResult() {
+        int resultado = Calculadora.getInstance().evaluate("1 2 +", new VectorStack<Integer>());
+        assertEquals(resultado, 3);
+    }
+
+    @Test
+    public void shouldReturnSingleLinkedListStackResult() {
+        int resultado = Calculadora.getInstance().evaluate("1 2 +", new StackFactory().getListStack("single"));
+        assertEquals(resultado, 3);
+    }
+
+    @Test
+    public void shouldReturnDoubleLinkedListStackResult() {
+        int resultado = Calculadora.getInstance().evaluate("1 2 +", new StackFactory().getListStack("double"));
+        assertEquals(resultado, 3);
     }
 }
